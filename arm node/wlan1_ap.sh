3df3e72dfbe69ae5bed6a5b9045bd7067ce4d13a
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-10-28 02:07:37 -0500

    fix for bridging issue

---
+++ arm node/wlan1_ap.sh
@@ -0,0 +1,20 @@
+# https://somesquares.org/blog/2017/10/Raspberry-Pi-router/
+# MUST RUN THIS SCRIPT AS SUDO
+
+apt-get install hostapd
+cat hostapd.conf > /etc/hostapd/hostapd.conf
+echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
+
+apt-get install dnsmasq
+cat dhcpcd.conf >> /etc/dhcpcd.conf
+cat dnsmasq.conf >> /etc/dnsmasq.conf
+
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+
+# wlan0 is source, wlan1 is AP
+iptables -t nat -A  POSTROUTING -o wlan0 -j MASQUERADE
+iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+
+apt-get install iptables-persistent
+
