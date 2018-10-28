ed00be5af08d68370233bf3bd58db865f61b195a
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-10-28 15:47:24 -0500

    fix for minor arm bugs

--- arm node/fix_routing.sh
+++ arm node/fix_routing.sh
@@ -1,3 +1,3 @@
 # https://raspberrypi.stackexchange.com/questions/71348/cannot-forward-from-wlan-interface-to-another-wlan-interface
 
-sudo route del -net 0.0.0.0 gw 192.168.1.1 netmask 0.0.0.0 dev wlan1
+sudo route del -net 0.0.0.0 dev wlan1
