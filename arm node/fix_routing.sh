1918b76d45cf3bf6132f3ab26b9089b2929b65f5
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-10-28 16:34:01 -0500

    Finally fixed SSH access

--- arm node/fix_routing.sh
+++
@@ -1,3 +0,0 @@
-# https://raspberrypi.stackexchange.com/questions/71348/cannot-forward-from-wlan-interface-to-another-wlan-interface
-
-sudo route del -net 0.0.0.0 dev wlan1
--- arm node/wlan1_ap.sh
+++ arm node/wlan1_ap.sh
@@ -17,8 +17,9 @@ echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 
 # wlan0 is source, wlan1 is AP
 iptables -t nat -A  POSTROUTING -o wlan0 -j MASQUERADE
-iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+# Don't need these...
+# iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
 
 apt-get install iptables-persistent
 
