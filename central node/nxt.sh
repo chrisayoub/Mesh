66959a371e83cf518a7f0bf597d183af3ccfb728
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-10-27 22:44:29 -0500

    updated nxt script

--- central node/nxt.sh
+++ central node/nxt.sh
@@ -1,9 +1,12 @@
 # http://www.lejos.org/nxt/nxj/tutorial/Preliminaries/GettingStartedLinux.htm
 
-sudo apt install libusb-dev
+sudo apt install libusb-dev ant
 
 echo 'PATH="/opt/lejos/bin:$PATH"' >> ~/.profile
 echo 'NXJ_HOME="/opt/lejos"' >>  ~/.profile
 
 mkdir /opt/lejos
-tar -C /opt/lejos -zxvf yourfile.tar.gz
+tar -zxvf lejos.tar.gz
+sudo mv -v ./leJOS_NXJ_0.9.1beta-3/ /opt/lejos
+
+( cd /opt/lejos/build && ant )
