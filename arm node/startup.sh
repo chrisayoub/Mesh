bab39a37664647c0be36b58234f3aa3b208fcebe
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-11-14 16:08:56 -0600

    Update startup.sh

--- arm node/startup.sh
+++ arm node/startup.sh
@@ -5,8 +5,7 @@ path=`pwd`
 dest='/etc/rc.local'
 
 echo '#!/bin/sh -e' > ${dest}
-echo "\n" > ${dest}
+echo "\n" >> ${dest}
 
 echo 'sudo python ${path}/server.py &' >> ${dest}
-echo "\n" > ${dest}
 echo 'exit 0' >> ${dest}
