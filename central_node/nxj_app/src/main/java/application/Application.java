a43aeb96efb389ba18c5447fbfc340264ba9d0d6
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-11-15 01:49:48 -0600

    Update Application.java

--- central_node/nxj_app/src/main/java/application/Application.java
+++ central_node/nxj_app/src/main/java/application/Application.java
@@ -1,5 +1,9 @@
 package application;
 
+import lejos.pc.comm.NXTComm;
+import lejos.pc.comm.NXTCommException;
+import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.comm.NXTInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +12,19 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("controller")
 @ComponentScan("service")
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NXTCommException {
+        // TEST CODE HERE
+        NXTComm c = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+        NXTInfo[] i = c.search(null);
+        if (i.length == 0) {
+            System.out.println("NO NXT!");
+            return;
+        }
+        for (NXTInfo ni : i) {
+            System.out.println(ni.name);
+        }
+
+        // Run here
         SpringApplication.run(Application.class, args);
     }
 }
