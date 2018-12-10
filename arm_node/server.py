52e1a416aa90b8e7865862039cc03c30ba78aa83
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-12-09 19:01:02 -0600

    Update server.py

--- arm_node/server.py
+++ arm_node/server.py
@@ -66,9 +66,7 @@ class myHandler(BaseHTTPRequestHandler):
 		data = str(os.popen(IP_CMD).read()).strip()
 		if data == '':
 			return
-		print(data)
 		ip = data.split(' ')[2]
-		print (ip)
 
 		PING_CMD = 'ping -c 1 -W 1 ' + ip
 		print(PING_CMD)
@@ -80,10 +78,8 @@ class myHandler(BaseHTTPRequestHandler):
 		while not q.empty(): # This value should be changed, hopefully
 			os.popen(PING_CMD)
 			time.sleep(RATE / 1000.0) # Milliseconds
-		print('Stopped!')
 
 	def stopPing(self):
-		print('stop ping')
 		if not self.q.empty():
 			self.q.get()
 
